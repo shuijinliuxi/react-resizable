6339cc507d48d7c92bc1542aa152fdc7c3e0f273
ChengOuyang <ouyangc@agilean.cn>
2022-03-10 12:00:01 +0800

    build

---
+++ build/utils.js
@@ -0,0 +1,27 @@
+"use strict";
+
+exports.__esModule = true;
+exports.cloneElement = cloneElement;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// React.addons.cloneWithProps look-alike that merges style & className.
+function cloneElement(element, props) {
+  if (props.style && element.props.style) {
+    props.style = _objectSpread(_objectSpread({}, element.props.style), props.style);
+  }
+
+  if (props.className && element.props.className) {
+    props.className = element.props.className + " " + props.className;
+  }
+
+  return /*#__PURE__*/_react.default.cloneElement(element, props);
+}
